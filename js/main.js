df76a35cc6839ae95a2cfbd2bf6799df875f2fcc
jessicafraines <jessicafraines@gmail.com>
2014-12-04 15:47:57 -0600

    made todo app out of this practice app

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 ;(function(){
   'use strict';
 
-  angular.module('myApp', [])
-    .controller('SimpleController', function(){
-      var vm = this;
-      vm.name = 'Scott';
-      vm.friends = ['Ashley', 'Bob', 'Carley', 'Dave'];
-    });
+  angular.module('todoApp', [])
+  .controller('TodoController', function(){
+    var vm = this;
+    vm.tasks = [
+      {
+        name: 'Learn Angular',
+        desc: 'If I could learn Angular I\'d be sooo happy',
+        due:  'Today'
+      },
+      {
+        name: 'Finish Tic Tac Toe',
+        desc: 'If I could finish Tic Tac Toe I\'d be sooo happy',
+        due:  'Monday'
+      },
+      {
+        name: 'Get a job',
+        desc: 'If I could get a job I\'d be sooo happy',
+        due:  'Tuesday'
+      }
+    ];
+
+    vm.addNewTask = function(){
+      vm.tasks.push(vm.newTask);
+      vm.newTask = null;
+    };
+
+    vm.deleteTask = function(task){
+      var index = vm.tasks.indexOf(task);
+      vm.tasks.splice(index, 1);
+    };
+  });
 
 }());
